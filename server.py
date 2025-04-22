c5f9016c45b771006fe56154f990837ed503947e
Sulamith K. Caceres Ranilla <skc2173.columbia.edu>
2025-04-22 19:40:29 -0400

    added route from home-->learning page

--- server.py
+++ server.py
@@ -9,5 +9,10 @@ app = Flask(__name__)
 def home():
    return render_template('home.html') 
 
+@app.route('/learning')
+def learning():
+    return render_template('learning.html')
+
+
 if __name__ == '__main__':
    app.run(debug = True, port=5001)
---
+++ static/quiz.js
@@ -0,0 +1,9 @@
+$( function() {
+    // start quiz button from home page --> learning
+    $("#start-quiz").on("click", function(event) {
+        window.location.href = `/learning`;
+    })
+
+    $(document).ready(function () {
+    })
+})
