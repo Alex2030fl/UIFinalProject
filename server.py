f0ed9b7c2e8af832ceab2336ebf730e5da77f5b2
Sulamith K. Caceres Ranilla <skc2173.columbia.edu>
2025-04-22 22:11:54 -0400

    added data, userflow in server.py + learn.html

--- server.py
+++ server.py
@@ -5,17 +5,48 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+progress_id = 1 # <--- where in the lecture the user is at
+lecture = {
+    "1": {
+        "id":"1",
+        "title":"2:3",
+        "video_link":"https://youtu.be/2teP7CIYoII?si=Hqxqy9yFV3lo3lRJ",
+        "text":"Watch this video. Try speaking the mnemonic device for 2:3: “nice cup of tea”",
+    },
+    "2": {
+        "id":"2",
+        "title":"3:4",
+        "video_link":"https://youtu.be/61W9VthQYHQ?si=jLUjyEKTd5ZSWyem",
+        "text":"Watch this video. Try speaking the mnemonic device for 3:4: “pass the golden butter”",
+    },
+    "3": {
+        "id":"3",
+        "title":"4:5",
+        "video_link":"https://youtu.be/2OCfwdANMkk?si=bH8O5W625RRTg4FL",
+        "text":"Watch this video. Try speaking the mnemonic device for 4:5: “I'm looking for a home to buy”",
+    },
+}
+
 @app.route('/')
 def home():
-   return render_template('home.html') 
+    return render_template('home.html') 
 
 @app.route('/quiz')
 def quiz():
     return render_template('quiz.html')
 
-@app.route('/learn')
-def learn():
-    return render_template('learn.html')
+@app.route('/learn/<id>', methods=['GET'])
+def learn(id):
+    # global progress_id
+    # progress_id += 1
+    current_lesson = lecture.get(id)
+    lesson_ids = sorted(lecture.keys())
+    current_index = lesson_ids.index(id) if id in lesson_ids else 0
+    
+    prev_lesson = lecture.get(lesson_ids[current_index - 1]) if current_index > 0 else None
+    next_lesson = lecture.get(lesson_ids[current_index + 1]) if current_index < len(lesson_ids) - 1 else None
+
+    return render_template('learn.html', current_lesson=current_lesson, prev_lesson=prev_lesson, next_lesson=next_lesson)
 
 
 if __name__ == '__main__':
--- static/quiz.js
+++ static/quiz.js
@@ -1,14 +1,18 @@
 $( function() {
-    // start quiz button from home page --> learning
+    // --------------------------------------------------------- HOME.HTML ----------------
+    // start quiz button from home page --> quiz
     $("#start-quiz").on("click", function(event) {
         window.location.href = `/quiz`;
     })
 
     // start learning button from home page --> learning
     $("#start-learning").on("click", function(event) {
-        window.location.href = `/learn`;
+        window.location.href = `/learn/1`; // hard coded id = 1 to start with 1st lesson
     })
+    
+    // --------------------------------------------------------- LEARN.HTML ----------------
 
     $(document).ready(function () {
+        
     })
 })
